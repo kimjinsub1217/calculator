a9bffb731e10f199fa2065ecb06d8962966addd3
kimjinsub <hikm9985@naver.com>
2022-01-04 20:13:32 +0900

    UI 그리기

---
+++ app/src/main/java/com/example/toyproject004_calculator/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.toyproject004_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClicked(v: View){
+
+    }
+
+    fun clearButtonClicked(v: View){
+
+    }
+
+    fun historyButtonClicked(v: View){
+
+    }
+
+    fun resultbuttonClicked(v: View){
+
+    }
+
+}
